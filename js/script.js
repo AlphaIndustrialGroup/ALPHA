2df8ed6b7e93b4f1272a6d5c004a4015c38899f2
AlphaIndustrialGroup <78916282+AlphaIndustrialGroup@users.noreply.github.com>
2021-02-18 06:22:03 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-alert("به سایت. گروه صنعتی آلفا خوش آمدید")
+alert("به وب سایت گروه صنعتی آلفا خوش آمدید.")
 $(document).ready(function () {
     'use strict';
 
     setTimeout(function () {
         $('.loader_bg').fadeToggle();
     }, 1500);
-alert("CREATE BAY:ABBAS AREZOMANDI")
+alert("کلیه  حقوق  و  مزایای  این  سایت  متعلق  به  آقای عباس آرزومندی میباشد:)")
     $(window).on('scroll', function () {
         if ($(document).scrollTop() > 200) {
             $(".navbar").css({ "background-color": "#ffffff", "transition": "all 0.8s ease-in-out", "box-shadow": "0px 3px 4.6px 0.3px rgba(0, 0, 0, 0.25)" });
@@ -18,7 +18,7 @@ alert("CREATE BAY:ABBAS AREZOMANDI")
     var element = $(".text-affect");
     $(function () {
         element.typed({
-            strings: ["Designer", "Developer", "Freelancer"],
+            strings: ["گروه صنعتی آلفا", "سازنده تجهیزات", "صنایع غذایی","دارویی و شیمیایی","با بهره مندی از ","تکنولوژی روز  و","کادری مجرب و","حرفه ایی  میباشد "],
             loop: true,
             typeSpeed: 90
         });
@@ -30,7 +30,7 @@ alert("CREATE BAY:ABBAS AREZOMANDI")
     });
 
     $('.view').magnificPopup({
-        type: 'imag@e',
+        type: 'image',
         gallery: {
             enabled: true
         },
@@ -42,3 +42,34 @@ alert("CREATE BAY:ABBAS AREZOMANDI")
             }
         }
     });
+
+    $("#review-slider").owlCarousel({
+        items: 1,
+
+        itemsDesktop: [1000, 1],
+
+        itemsDesktopSmall: [979, 1],
+
+        itemsTablet: [768, 1],
+
+        pagination: true,
+
+        navigation: false,
+
+        slideSpeed: 1000,
+
+        singleItem: false,
+
+        autoplay: true,
+
+        loop: true
+    });
+    
+    $("#collapsibleNavbar").click(function(){
+    $("#collapsibleNavbar").hide();
+  });
+    $("#colnav").click(function(){
+    $("#collapsibleNavbar").toggle();
+  });
+
+});
